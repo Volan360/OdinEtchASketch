ba792374f2304497eacdaac2860b96b388e5e016
Abdelrahman <avalonsallam@gmail.com>
2022-08-07 16:09:11 -0700

    Clean up UI and add logic for sketching

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 row_counts = [10,16,18]
+
+// prevent dragging and dropping
+const div = document.querySelector('div')
+div.addEventListener('dragstart', (e) => {
+  e.preventDefault()
+})
+
+div.addEventListener('drop', (e) => {
+  e.preventDefault()
+})
+//check if mouse is down
+window.md = 0;  
+window.onmousedown = () => {  
+  window.md = 1;
+  console.log(md)
+}  
+window.onmouseup = () => {  
+  window.md = 0;   
+}
+
+//makes the grid and adds cell logic
 function make_grid(grid_size, cell_size)
 {
     cell_size_with_border = cell_size + 2
@@ -13,6 +34,12 @@ function make_grid(grid_size, cell_size)
         {
             let cell = document.createElement('div')
             cell.classList.add('cell')
+            cell.draggable = false
+            cell.onmouseover = function (md) {
+                if(window.md == 1) {
+                    cell.style['background-color'] = 'black'
+                }
+            }
             cell.style['height'] = cell_size.toString() + 'px'
             cell.style['width'] = cell_size.toString() + 'px'
             grid.appendChild(cell)
@@ -20,13 +47,13 @@ function make_grid(grid_size, cell_size)
     }
 }
 make_grid(720, 18)
+
+//makes the buttons able to change grid density
 let size_buttons = document.getElementById("size_buttons")
 for(i of row_counts){
     let button = document.createElement("button")
-    button.textContent = i.toString() + 'x' + i.toString()
+    button.textContent = i.toString() + 'px'
     button.onclick = () => make_grid(720, parseInt(button.textContent))
+    button.classList.add('button-50')
     size_buttons.appendChild(button)
 }
-
-// container.style['width'] = i.toString() + 'px'
-// container.style['height'] = j.toString() + 'px'
